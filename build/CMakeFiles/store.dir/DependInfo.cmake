943c26fea4d9a72cf39d8cc49580c341d4b5b6a4
Zouhair <moncif.zouhair.61@edu.uiz.ac.ma>
2026-05-21 22:49:38 +0100

    add Cmake to project

---
+++ build/CMakeFiles/store.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/moncifzouhair/Desktop/ecommerce/main.cpp" "CMakeFiles/store.dir/main.cpp.o" "gcc" "CMakeFiles/store.dir/main.cpp.o.d"
+  "/home/moncifzouhair/Desktop/ecommerce/src/Cart.cpp" "CMakeFiles/store.dir/src/Cart.cpp.o" "gcc" "CMakeFiles/store.dir/src/Cart.cpp.o.d"
+  "/home/moncifzouhair/Desktop/ecommerce/src/Clothes.cpp" "CMakeFiles/store.dir/src/Clothes.cpp.o" "gcc" "CMakeFiles/store.dir/src/Clothes.cpp.o.d"
+  "/home/moncifzouhair/Desktop/ecommerce/src/InformaticsEquipment.cpp" "CMakeFiles/store.dir/src/InformaticsEquipment.cpp.o" "gcc" "CMakeFiles/store.dir/src/InformaticsEquipment.cpp.o.d"
+  "/home/moncifzouhair/Desktop/ecommerce/src/KitchenEquipment.cpp" "CMakeFiles/store.dir/src/KitchenEquipment.cpp.o" "gcc" "CMakeFiles/store.dir/src/KitchenEquipment.cpp.o.d"
+  "/home/moncifzouhair/Desktop/ecommerce/src/Others.cpp" "CMakeFiles/store.dir/src/Others.cpp.o" "gcc" "CMakeFiles/store.dir/src/Others.cpp.o.d"
+  "/home/moncifzouhair/Desktop/ecommerce/src/Product.cpp" "CMakeFiles/store.dir/src/Product.cpp.o" "gcc" "CMakeFiles/store.dir/src/Product.cpp.o.d"
+  "/home/moncifzouhair/Desktop/ecommerce/src/Store.cpp" "CMakeFiles/store.dir/src/Store.cpp.o" "gcc" "CMakeFiles/store.dir/src/Store.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/store.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/store.dir/main.cpp.o"
+  "CMakeFiles/store.dir/main.cpp.o.d"
+  "CMakeFiles/store.dir/src/Cart.cpp.o"
+  "CMakeFiles/store.dir/src/Cart.cpp.o.d"
+  "CMakeFiles/store.dir/src/Clothes.cpp.o"
+  "CMakeFiles/store.dir/src/Clothes.cpp.o.d"
+  "CMakeFiles/store.dir/src/InformaticsEquipment.cpp.o"
+  "CMakeFiles/store.dir/src/InformaticsEquipment.cpp.o.d"
+  "CMakeFiles/store.dir/src/KitchenEquipment.cpp.o"
+  "CMakeFiles/store.dir/src/KitchenEquipment.cpp.o.d"
+  "CMakeFiles/store.dir/src/Others.cpp.o"
+  "CMakeFiles/store.dir/src/Others.cpp.o.d"
+  "CMakeFiles/store.dir/src/Product.cpp.o"
+  "CMakeFiles/store.dir/src/Product.cpp.o.d"
+  "CMakeFiles/store.dir/src/Store.cpp.o"
+  "CMakeFiles/store.dir/src/Store.cpp.o.d"
+  "store"
+  "store.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/store.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
